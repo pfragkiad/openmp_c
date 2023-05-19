d137e35323e98a2de64029b9b53080c7f2a29414
pfragkiad <pfragkiad@auth.gr>
2023-05-20 01:58:11 +0300

    Add threads count.

--- main.c
+++ main.c
@@ -6,18 +6,21 @@
 
 int main(int argc, char *argv[])
 {
-    //OMP_NUM_THREADS is the external environment variable
-    //in Ubuntu:
-    //export OMP_NUM_THREADS = 10
-    //echo $OMP_NUM_THREADS
+    // OMP_NUM_THREADS is the external environment variable
+    // in Ubuntu:
+    // export OMP_NUM_THREADS = 10
+    // echo $OMP_NUM_THREADS
 
-omp_set_num_threads(18); //thread id from 0 to 17
+    omp_set_num_threads(18); // thread id from 0 to 17
 
 #pragma omp parallel
     {
-       printf("%d hello ",omp_get_thread_num());
-       printf(" world \n");
-    } //that's a barrier here!
+        int id = omp_get_thread_num();
+        int threadsCount = omp_get_num_threads();
+
+        printf("(%d of %d) hello ", id, threadsCount);
+        printf(" world \n");
+    } // that's a barrier here!
 
     return EXIT_SUCCESS;
 }
